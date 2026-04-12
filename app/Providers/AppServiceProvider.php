3e634f9a944b0265f6bde4dd177b9767e9b0a77f
AjEmKocur <dominikkocur12@gmail.com>
2026-04-12 21:55:16 +0200

    Revert "Force HTTPS scheme in production for asset URLs"
    This reverts commit d1cf1b3a8e65fd25227fcb8911887665d1e78b50.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -21,10 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
-
         Password::defaults(function () {
             return Password::min(8)
                 ->letters()
