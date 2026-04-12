2dfc6c76fef4a365d724fb40993e93bdcc5f9f75
AjEmKocur <dominikkocur12@gmail.com>
2026-04-12 21:55:16 +0200

    Revert "Add CSRF mismatch diagnostics for Render login issue"
    This reverts commit f67c9966983280c090a72b17f935806f4a0440fa.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Session\TokenMismatchException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -34,22 +33,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->report(function (TokenMismatchException $exception, Request $request): void {
-            logger()->warning('CSRF token mismatch on request.', [
-                'method' => $request->method(),
-                'path' => $request->path(),
-                'full_url' => $request->fullUrl(),
-                'has_session_cookie' => $request->cookies->has(config('session.cookie')),
-                'session_cookie_name' => config('session.cookie'),
-                'session_id' => $request->hasSession() ? $request->session()->getId() : null,
-                'has_csrf_input' => $request->request->has('_token'),
-                'has_x_csrf_header' => $request->headers->has('X-CSRF-TOKEN'),
-                'has_x_xsrf_header' => $request->headers->has('X-XSRF-TOKEN'),
-                'origin' => $request->headers->get('origin'),
-                'referer' => $request->headers->get('referer'),
-                'host' => $request->getHost(),
-                'is_secure' => $request->isSecure(),
-                'forwarded_proto' => $request->headers->get('x-forwarded-proto'),
-            ]);
-        });
+        //
     })->create();
