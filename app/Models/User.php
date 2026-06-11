d0ae6ad4228d59891c793b0725bc072db1172905
AjEmKocur <dominikkocur12@gmail.com>
2026-06-11 20:30:26 +0200

    Clean unused project scaffold files

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,13 @@ namespace App\Models;
 
 use App\Notifications\VerifyEmailNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use Notifiable;
 
     public const ROLE_ADMIN = 'admin';
     public const ROLE_OPERATOR = 'operator';
